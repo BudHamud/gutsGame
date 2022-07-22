c0f9fd46711cfdef8bfd2bf1741268a1f922575e
BudHamud <nigitart@gmail.com>
2022-07-21 22:20:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let sec = document.querySelector(".sechis")
+let img = document.querySelector(".imghis")
+let opcion = 0
+
+acciones()
+
+function acciones() {
+    botonSig()
+}
+
+function botonSig() {
+    const btn = document.getElementById("btn")
+    btn.addEventListener('click', ()=> {
+        fech()
+    })
+}
+
+function fech() {
+    fetch('api.json')
+    .then(resp => resp.json())
+    .then(json => mostrarTexto(json))
+} 
+
+function mostrarTexto(textz) {
+    img.innerHTML =`<img src="img/img${textz[opcion].cinco}.png">`
+    sec.innerHTML=
+    `<h2>${textz[opcion].titulo}</h2>
+    <p>${textz[opcion].texto}</p>
+    <button onclick="cambiar(value)" value=${textz[opcion].tres}>${textz[opcion].uno}</button>
+    <button onclick="cambiar(value)" value=${textz[opcion].cuatro}>${textz[opcion].dos}</button>
+    `
+}
+
+function cambiar(value) {
+    opcion = value
+    fech()
+}
